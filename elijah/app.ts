f685f824b6a11fa4d2580ae5a6b80391bbdbff73
Elijah <elitkramer@gmail.com>
2024-04-22 16:56:20 -0700

    Better document code with comments

--- elijah/app.ts
+++ elijah/app.ts
@@ -33,6 +33,8 @@ const getIsPostVisible = (postId: string, username: string) => {
   const author = findUserByUsername(post.userId);
   const user = findUserByUsername(username);
 
+  // Posts are visible if they are public, authored by
+  // this user, or are authored by someone who has this user as a friend.
   let hasAccess = post.visibility === "public";
   hasAccess = hasAccess || author.username === user.username;
   hasAccess = hasAccess || author.friends.includes(user.username);
@@ -40,7 +42,9 @@ const getIsPostVisible = (postId: string, username: string) => {
   return hasAccess;
 };
 
+// 1. Load input data.
 const optionLoadInputData = async (rl: ReadlineInterface) => {
+  // Ask for post and user info paths
   let postInfoPath = await rl.question(
     "Path to post-info.txt (data/post-info.txt): "
   );
@@ -48,6 +52,7 @@ const optionLoadInputData = async (rl: ReadlineInterface) => {
     "Path to user-info.txt (data/user-info.txt): "
   );
 
+  // If we don't input specific paths, assume these as the default for convenience
   if (!postInfoPath) {
     postInfoPath = "data/post-info.txt";
   }
@@ -55,9 +60,11 @@ const optionLoadInputData = async (rl: ReadlineInterface) => {
     userInfoPath = "data/user-info.txt";
   }
 
+  // Read both files
   const postInfoFile = (await readFile(postInfoPath)).toString();
   const userInfoFile = (await readFile(userInfoPath)).toString();
 
+  // Map each line to an object of type Post
   posts = postInfoFile.split("\n").map((line) => {
     const [postId, userId, visibility] = line.split(";");
     const post: Post = {
@@ -69,12 +76,14 @@ const optionLoadInputData = async (rl: ReadlineInterface) => {
     return post;
   });
 
+  // Map each line to an object of type User
   users = userInfoFile.split("\n").map((line) => {
     const [username, displayName, state, friendsList] = line.split(";");
     const user: User = {
       username,
       displayName,
       state,
+      // Remove braces and turn into an array
       friends: friendsList.split("[")[1].split("]")[0].split(","),
     };
 
@@ -82,6 +91,7 @@ const optionLoadInputData = async (rl: ReadlineInterface) => {
   });
 };
 
+// 2. Check visibility.
 const optionCheckVisibility = async (rl: ReadlineInterface) => {
   const postId = await rl.question("Post id: ");
   const username = await rl.question("Username: ");
@@ -90,19 +100,23 @@ const optionCheckVisibility = async (rl: ReadlineInterface) => {
   console.log(isPostVisible ? "Access Permitted" : "Access Denied");
 };
 
+// 3. Retrieve posts.
 const optionRetrievePosts = async (rl: ReadlineInterface) => {
   const username = await rl.question("Username: ");
   const visiblePosts = posts.filter((o) =>
     getIsPostVisible(o.postId, username)
   );
 
+  // Print as JSON-style array
   console.log(JSON.stringify(visiblePosts.map((o) => o.postId)));
 };
 
+// 4. Search users by location.
 const optionSearchByLocation = async (rl: ReadlineInterface) => {
   const state = await rl.question("State: ");
   const usersWithState = users.filter((user) => user.state == state);
 
+  // Print as JSON-style array
   console.log(JSON.stringify(usersWithState.map((o) => o.displayName)));
 };
 
@@ -113,8 +127,11 @@ const entry = async () => {
   });
 
   let shouldContinue = true;
+
+  // Continue prompting until exit command is given
   while (shouldContinue) {
-    const choice = await rl.question(`
+    // Ask user to input command
+    const command = await rl.question(`
 1. Load input data.
 2. Check visibility.
 3. Retrieve posts.
@@ -122,7 +139,7 @@ const entry = async () => {
 5. Exit.
 Choice: `);
 
-    switch (choice) {
+    switch (command) {
       case "1": {
         await optionLoadInputData(rl);
         break;
